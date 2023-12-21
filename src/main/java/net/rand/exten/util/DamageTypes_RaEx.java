278bfe1a5e11a77134fd0107b3921f2be091acea
luca2 <luca2006.mu@gmail.com>
2023-12-21 20:47:01 +0100

    added Pebbles Damage Type

--- src/main/java/net/rand/exten/util/DamageTypes_RaEx.java
+++ src/main/java/net/rand/exten/util/DamageTypes_RaEx.java
@@ -11,6 +11,7 @@ public class DamageTypes_RaEx {
 
     public static final RegistryKey<DamageType> BARBED_WIRE_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier("randexten", "barbed_wire_dt"));
     public static final RegistryKey<DamageType> END_BARY_BUSH_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier("randexten", "end_berry_bush_dt"));
+    public static final RegistryKey<DamageType> PEBBLE_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier("randexten", "pebble_dt"));
 
     public static DamageSource of(World world, RegistryKey<DamageType> key) {
         return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
