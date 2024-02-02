e5ce990ff393b91d2353da99d72dd8c3f3b2e4bf
luca2 <luca2006.mu@gmail.com>
2024-02-02 17:40:28 +0100

    TopHat

--- src/main/java/net/rand/exten/item/custom/wearable/accessories/head/TopTopHat.java
+++ src/main/java/net/rand/exten/item/custom/wearable/accessories/head/TopTopHat.java
@@ -1,13 +1,32 @@
 package net.rand.exten.item.custom.wearable.accessories.head;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.rand.exten.item.custom.wearable.Hats;
 
 public class TopTopHat extends Hats {
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
     public TopTopHat(Settings settings) {
         super(settings);
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier("Accessory modifier", 1, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_MAX_HEALTH, new EntityAttributeModifier("Accessory modifier", 2, EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        if (slot == EquipmentSlot.HEAD) {
+            return this.attributeModifiers;
+        }
+        return super.getAttributeModifiers(slot);
     }
 
     @Override
